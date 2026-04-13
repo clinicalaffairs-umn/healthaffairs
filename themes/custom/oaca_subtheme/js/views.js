6b13e32e08218e570517ad29f1fa7eaf048ef8c6
Adam Fuchs <adam@electriccitizen.com>
2026-04-13 12:33:20 -0500

    Set up block layout, adjust theming for new block ID setup, fix select2

--- themes/custom/oaca_subtheme/js/views.js
+++ themes/custom/oaca_subtheme/js/views.js
@@ -4,8 +4,8 @@
 	------------------ */
 	Drupal.behaviors.filterResultsHeaders = {
 	  attach: function (context, settings) {
-			$(once('hasValue', '#edit-field-categories-target-id option:selected', context)).each(function(){
-	      var searchTerm = $(this).text();
+			$(once('hasValue', '#edit-field-categories-target-id', context)).each(function(){
+	      var searchTerm = $(this).find('option:selected').text();
 	      if(searchTerm != '- Any -'){
 	      	$('.categories-place').html('<span>' + searchTerm + '</span>');
 	    	}
